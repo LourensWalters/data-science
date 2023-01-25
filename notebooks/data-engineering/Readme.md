6d8e75377ecf3547b6159da1d8445b92c4cea1ea
LourensWalters <48653822+LourensWalters@users.noreply.github.com>
2023-01-25 21:26:43 +0000

    Updates

---
+++ notebooks/data-engineering/Readme.md
@@ -0,0 +1,61 @@
+
+# Overview
+The purpose of this project is to build machine learning pipeline, which consists of Natural Language Processing model and Classification model, to categorize disaster response messages appropriately according to which disaster relief agency the message should be sent to. The project includes a web app which an emergency worker can use to input a message to obtain a classification indicating where the message should be sent to.
+
+# Components
+The project is divided into three components:
+
+1. **ETL Pipeline:** Load datasets, clean the data and store it in a SQLite database
+2. **ML Pipeline:** Build a text processing and machine learning pipeline, train a model to classify text message in categories
+3. **Flask Web App:** Show model results in real time
+
+# Requirements
+- Python 3.10
+- Machine Learning Libraries: NumPy, SciPy, Pandas, Sciki-Learn
+- Natural Language Process Libraries: NLTK
+- SQLlite Database Libraqries: SQLalchemy
+
+# Instructions:
+1. Run the following commands in the project's root directory to set up the database and model.
+
+    - To run ETL pipeline that cleans data and stores in database
+      `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/disaster.db`
+    - To run ML pipeline that trains classifier and saves
+      `python models/train_classifier.py data/disaster.db models/classifier.pkl`
+
+2. Run the following command in the app's directory to run the web app.
+   `python run.py`
+
+3. Go to http://0.0.0.0:3001/
+
+
+# Classification Model
+The dataset is imbalanced (ie some labels like water have few examples) which contributes to lower score for the ones which have less data. That is why the overall score is low. For skewed datasets, accuracy as a metric for model evaluation is not preferred. In this case, it's better to have more FP than FN, as it makes sure than the messages are at least sent to appropriate disaster relief agency, instead of ignoring some really urgent messages (FN). Therefore, it is important to have a classification model that shows low number of FN -> high recall value.
+
+<p align="center">
+  <img src="/images/classification_report.png" height="600" width="800" />
+</p>
+
+# Acknowledgements
+- [Udacity](https://www.udacity.com) for providing such a interesting and meaningful project
+- [Figure Eight](https://appen.com) for providing real-world dataset
+
+# Results
+1. Input message to get classification results
+<p align="center">
+  <img src="/images/message_input.png" height="200" width="800" />
+</p>
+
+2. Example: The categories which the message belongs to highlighted in green
+<p align="center">
+  <img src="/images/result_example.png" height="600" width="800" />
+</p>
+
+3. Overview of Training Dataset
+<p align="center">
+  <img src="/images/distribution_genre.png" height="400" width="800" />
+</p>
+
+<p align="center">
+  <img src="/images/distribution_catogory.png" height="400" width="800" />
+</p>
---
+++ notebooks/data-engineering/app/run.py
@@ -0,0 +1,146 @@
+import json
+import plotly
+import pandas as pd
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet') # download for lemmatization
+nltk.download("averaged_perceptron_tagger")
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+import re
+import matplotlib.pyplot as plt
+import pickle
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report, make_scorer, f1_score
+
+
+app = Flask(__name__)
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# load data
+engine = create_engine('sqlite:///../data/disaster.db')
+df = pd.read_sql_table('model_data4', engine)
+
+# load model
+model = joblib.load("../models/disaster_ada_model.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        }
+    ]
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '')
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file.
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3000, debug=True)
+
+
+if __name__ == '__main__':
+    main()
